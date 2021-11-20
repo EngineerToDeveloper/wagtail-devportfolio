fa0baddaa503ff34e03a2cba947e78b04b5a09e1
Brian <semperindoctrina19@gmail.com>
2021-11-19 20:27:42 -0600

    Tutorial 2

---
+++ devportfolio/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-(csbgsbl3zzu=igb#_^29_^=6ny!6i6!b$u81#dkc_0r@8%b)#'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ home/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+
+
+class HomePage(Page):
+    hero_title = models.CharField(
+        max_length=120,
+        blank=True,
+        help_text="Main text displayed in the hero section over the background."
+    )
+
+    hero_subtitle = models.TextField(
+        max_length=200,
+        blank=True,
+        help_text="Subtitle following the main title in the hero section."
+    )
+
+    cta_btn_text = models.CharField(
+        max_length=20,
+        blank=True,
+        default="Learn More",
+        help_text="Call-To-Action Button Text",
+    )
+
+    cta_btn_link = models.ForeignKey(
+        'wagtailcore.page',
+        null=True,
+        blank=True,
+        related_name="+",
+        on_delete=models.SET_NULL,
+        help_text="Internal page link to send the user to when clicking CTA button."
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("hero_title"),
+        FieldPanel("hero_subtitle"),
+        FieldPanel("cta_btn_text"),
+        PageChooserPanel("cta_btn_link"),
+    ] 
